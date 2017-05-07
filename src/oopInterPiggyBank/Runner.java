970e264809002e7382116f045d9c62e076a2dc04
ArunAnnaldas <cmeatarun1988@gmail.com>
2017-05-07 14:18:09 +0530

    Added the object creation logic into runner class

--- src/oopInterPiggyBank/Runner.java
+++ src/oopInterPiggyBank/Runner.java
@@ -4,7 +4,12 @@ public class Runner {
 
 	public static void main(String[] args) {
 
+		OOPPiggyBank_Model pg1 = new OOPPiggyBank_Model();
+		OOPPiggyBank_Model pg2 = new OOPPiggyBank_Model();
+		OOPPiggyBank_Model pg3 = new OOPPiggyBank_Model();
+		OOPPiggyBank_Model pg4 = new OOPPiggyBank_Model();
+
 		Controller c1 = new Controller();
-		c1.execute();
+		c1.execute(pg1,pg2,pg3,pg4);
 	}
 }
